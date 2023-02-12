8820e308ae6c37704166ccd2ac632e86d70a839d
Sujit Memane <officialsujitmemane@gmail.com>
2023-02-12 22:45:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function (e) {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
